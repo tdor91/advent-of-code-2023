1dc2f1f47e02957a46e1bc3fca6d83945a5c7421
Thomas Dorfer <thomas.dorfer@ktm.at>
2023-12-14 12:50:11 +0100

    add solution for day 14 (part 1)

---
+++ src/Day14/Program.cs
@@ -0,0 +1,48 @@
+﻿var input = new Input("input.txt");
+
+var matrix = input.Matrix;
+TiltNorth(matrix);
+
+var load = matrix
+    .Reverse()
+    .Select((row, i) => row.Count(c => c == 'O') * (i + 1))
+    .Sum();
+Console.WriteLine(load);
+
+
+void TiltNorth(char[][] matrix)
+{
+    int[] spaces = new int[matrix[0].Length];
+
+    for (int y = 0; y < matrix.Length; y++)
+    {
+        for (int x = 0; x < matrix[y].Length; x++)
+        {
+            switch (matrix[y][x])
+            {
+                case '.':
+                    spaces[x]++;
+                    break;
+                case '#':
+                    spaces[x] = 0;
+                    break;
+                case 'O':
+                    matrix[y][x] = '.';
+                    matrix[y - spaces[x]][x] = 'O';
+                    break;
+                default: throw new Exception();
+            }
+        }
+    }
+}
+
+class Input
+{
+    public Input(string path)
+    {
+        var lines = File.ReadAllLines(path);
+        Matrix = lines.Select(line => line.ToCharArray()).ToArray();
+    }
+
+    public char[][] Matrix { get; }
+}
